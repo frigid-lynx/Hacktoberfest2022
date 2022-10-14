ac4d3bef550723c1403944c2b393704057964910
Tanu Aggarwal <tanuaggarwal0123@gmail.com>
2022-10-14 11:47:01 +0530

    Update ProductOfNumTerms.java

--- ProductOfNumTerms.java
+++ ProductOfNumTerms.java
@@ -5,7 +5,7 @@ class Prod{
         int n = sc.nextInt();
         int[] arr = new int[n];
         int prod=1;
-        for(int i=0;i<n;i++){arr[i]=sc.nextInt();sum*=arr[i];}
-        System.out.println("Product of "+n+" terms: "+sum);
+        for(int i=0;i<n;i++){arr[i]=sc.nextInt();prod*=arr[i];}
+        System.out.println("Product of "+n+" terms: "+prod);
     }
 }
