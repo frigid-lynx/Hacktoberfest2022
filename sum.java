927571de86ed742dbfcde35561445df2f5a845d6
yukti_kash <kashyapyukti12@gmail.com>
2023-09-08 16:32:07 +0530

    done

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum
 public static void main(String args[])   
 {  
 int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+sum = n1 + n2+2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  
